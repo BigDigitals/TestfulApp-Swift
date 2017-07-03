b00089a1aca38e410d157d1e830f8aa9b0686742
Evgeniy Tkachenko <tkachenko-ei@MacBook-Air-Evgeniy.local>
2017-07-03 13:07:13 +0300

    first commit

---
+++ TestfulApp/Supports/CustomSegue.swift
@@ -0,0 +1,9 @@
+//
+//  CustomSegue.swift
+//  TestfulApp
+//
+//  Created by Evgeniy Tkachenko on 03.07.17.
+//  Copyright © 2017 Big Dig. All rights reserved.
+//
+
+import Foundation
---
+++ TestfulApp/Views/ClientLastMessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClientLastMessageCell.swift
+//  TestfulApp
+//
+//  Created by Evgeniy Tkachenko on 03.07.17.
+//  Copyright © 2017 Big Dig. All rights reserved.
+//
+
+import UIKit
+
+class ClientLastMessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
